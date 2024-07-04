ab34547bb62cc64e559a9e67ba234069b1fe8481
Vaibhav262610 <branda56235@gmail.com>
2024-07-04 10:21:43 +0530

    hello

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ReduxTodo from "./components/ReduxTodo";
+
+function App() {
+  return (
+    <>
+      <ReduxTodo />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ReduxTodo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ReduxTodo = () => {
+  return (
+    <>
+        <h1>HELLO REDUX</h1>
+    </>
+  )
+}
+
+export default ReduxTodo
